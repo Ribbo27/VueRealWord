b2cf5555c8ddf75238eed9ef9b4f5010852919dc
Ribbo <f.ribaudo1@campus.unimib.it>
2019-07-06 23:33:22 +0200

    milano_eventi_v0.1
    New Features added:
     - UX enhancement
     - Graphics enhancement

---
+++ src/components/EventCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} - {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} partecipanti</span>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        id: 1,
+        title: 'Hawaian party',
+        date: 'Sabato 29 luglio 2019',
+        time: '23:00',
+        attendees: [
+          {
+            id: 'abc123',
+            name: 'Ribbo'
+          },
+          {
+            id: 'def456',
+            name: 'Sprea'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Eventi</h1>
+    <EventCard />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  props: ['id'],
+  components: {
+    EventCard
+  }
+}
+</script>
