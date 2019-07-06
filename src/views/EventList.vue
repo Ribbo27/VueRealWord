521a521ef3af5af1da55abe1bd7a8dea6e3db66a
Ribbo <f.ribaudo1@campus.unimib.it>
2019-07-06 23:58:50 +0200

    Event-show dynamic route added

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Event List</h1>
+  <div>
+    <h1>Event List</h1>
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
+      >First Event #{{ id }}</router-link
+    >
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
