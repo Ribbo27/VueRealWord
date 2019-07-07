e9b95614b662ee0cc9589c1a4a2757c3759eb089
Ribbo <f.ribaudo1@campus.unimib.it>
2019-07-07 04:40:00 +0200

    Application state added

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Ciao {{ user.name }}, crea un evento</h1>
     <p>Creato da: {{ user.name }}</p>
-    <p>Categorie disponibili: {{ catLength }}</p>
+    <p>Evento JSON: {{ getEventById(2) }}</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
@@ -10,13 +10,11 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   computed: {
-    catLength() {
-      return this.$store.getters.catLength
-    },
+    ...mapGetters(['getEventById']),
     ...mapState(['user', 'categories'])
   }
 }
