8a9e4bc1633bbeb2f68226a6e7210ca5422f525b
Ribbo <f.ribaudo1@campus.unimib.it>
2019-07-07 17:47:38 +0200

    Event module created

---
+++ src/store/modules/event.js
@@ -0,0 +1,61 @@
+import EventService from '@/services/EventService.js'
+
+export const namespaced = true
+
+export const state = {
+  events: [],
+  eventsTotal: 0,
+  event: {}
+}
+
+export const mutations = {
+  ADD_EVENT(state, event) {
+    state.events.push(event)
+  },
+  SET_EVENTS(state, events) {
+    state.events = events
+  },
+  SET_EVENTS_TOTAL(state, eventsTotal) {
+    state.eventsTotal = eventsTotal
+  },
+  SET_EVENT(state, event) {
+    state.event = event
+  }
+}
+export const actions = {
+  createEvent({ commit, rootState }, event) {
+    console.log("L'utente " + rootState.user.user.name + ' ha creato un evento')
+    return EventService.postEvent(event).then(() => {
+      commit('ADD_EVENT', event)
+    })
+  },
+  fetchEvents({ commit }, { perPage, page }) {
+    EventService.getEvents(perPage, page)
+      .then(response => {
+        commit('SET_EVENTS_TOTAL', parseInt(response.headers['x-total-count']))
+        commit('SET_EVENTS', response.data)
+      })
+      .catch(error => {
+        console.log('Error - fetchEvents - ' + error.response.data.message)
+      })
+  },
+  fetchEvent({ commit, getters }, id) {
+    var event = getters.getEventById(id)
+    if (event) {
+      commit('SET_EVENT', event)
+    } else {
+      EventService.getEvent(id)
+        .then(response => {
+          commit('SET_EVENT', response.data)
+        })
+        .catch(error => {
+          console.log('Error - fetchEvent - ' + error.response.data.message)
+        })
+    }
+  }
+}
+export const getters = {
+  getEventById: state => id => {
+    return state.events.find(event => event.id === id)
+  }
+}
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -62,7 +62,7 @@ export default {
   methods: {
     createEvent() {
       this.$store
-        .dispatch('createEvent', this.event)
+        .dispatch('event/createEvent', this.event)
         .then(() => {
           this.$router.push({
             name: 'event-show',
