95cfce4a97879bb926b8c91ceb5760180a8ca0f2
Ribbo <f.ribaudo1@campus.unimib.it>
2019-07-07 04:27:30 +0200

    Getters implemented

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    <h1>Ciao {{ user.name }}, crea un evento</h1>
+    <p>Creato da: {{ user.name }}</p>
+    <p>Categorie disponibili: {{ catLength }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    catLength(){
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>
